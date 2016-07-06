7bf0ded0cc66814c75b8a7876214448ce6632b8b
Victor Chong <victor.chong@linaro.org>
2016-07-06 22:08:23 +0900

    dbg2

--- core/include/spi.h
+++ core/include/spi.h
@@ -38,17 +38,8 @@ enum spi_mode {
 };
 
 // TODO: How to + struct pl022_cfg *cfg here?
-struct spi_ops {
-	void (*txrx8)(struct pl022_cfg *cfg, uint8_t *wdat, uint8_t *rdat, uint32_t num_txpkts, uint32_t *num_rxpkts);
-	void (*txrx16)(struct pl022_cfg *cfg, uint16_t *wdat, uint16_t *rdat, uint32_t num_txpkts, uint32_t *num_rxpkts);
-	void (*tx8)(struct pl022_cfg *cfg, uint8_t *wdat, uint32_t num_txpkts);
-	void (*tx16)(struct pl022_cfg *cfg, uint16_t *wdat, uint32_t num_txpkts);
-	void (*rx8)(struct pl022_cfg *cfg, uint8_t *rdat, uint32_t *num_rxpkts);
-	void (*rx16)(struct pl022_cfg *cfg, uint16_t *rdat, uint32_t *num_rxpkts);
-};
-
 struct pl022_cfg {
-	struct spi_ops	ops;
+	struct spi_ops	*ops;
 	vaddr_t			base;
 	vaddr_t			cs_gpio_base; /* gpio register base address for chip select */
 	uint32_t		clk_hz;
@@ -57,6 +48,15 @@ struct pl022_cfg {
 	uint8_t			mode;
 	uint8_t			data_size_bits;
 	bool			loopback;
+};
+
+struct spi_ops {
+	void (*txrx8)(struct pl022_cfg *cfg, uint8_t *wdat, uint8_t *rdat, uint32_t num_txpkts, uint32_t *num_rxpkts);
+	void (*txrx16)(struct pl022_cfg *cfg, uint16_t *wdat, uint16_t *rdat, uint32_t num_txpkts, uint32_t *num_rxpkts);
+	void (*tx8)(struct pl022_cfg *cfg, uint8_t *wdat, uint32_t num_txpkts);
+	void (*tx16)(struct pl022_cfg *cfg, uint16_t *wdat, uint32_t num_txpkts);
+	void (*rx8)(struct pl022_cfg *cfg, uint8_t *rdat, uint32_t *num_rxpkts);
+	void (*rx16)(struct pl022_cfg *cfg, uint16_t *rdat, uint32_t *num_rxpkts);
 };
 
 void spi_test2(void);
