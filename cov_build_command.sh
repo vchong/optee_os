9ab905d56fbd5dbcc36c3bde64bd8e599d6d38b6
Victor Chong <victor.chong@linaro.org>
2016-11-30 08:01:47 +0000

    use build script again

--- cov_build_command.sh
+++ cov_build_command.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-make
-make PLATFORM=hikey CFG_ARM64_core=y
+echo "Enter coverity_build.sh"
+
+set -ev
+
+cd $HOME/build/vchong/optee_os
+export PATH=${PWD}/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/bin:${PATH}
+export PATH=${PWD}/gcc-linaro-5.3-2016.02-x86_64_aarch64-linux-gnu/bin:${PATH}
+export MYHOME=$PWD
+export DST_KERNEL=$PWD/linux
+export DL_DIR=$HOME/downloads
+export make="make -j3 -s"
+export PATH=${HOME}/bc-1.06/bc:$PATH
+export PATH=$HOME/inst/bin:$PATH
+export PATH=$HOME/bin:$PATH
+export PATH=$HOME/git-2.9.3/:$DST_KERNEL/scripts/:$PATH
+
+# QEMU-virt (PLATFORM=vexpress-qemu_virt)
+$make
+
+# HiKey board (HiSilicon Kirin 620)
+$make PLATFORM=hikey CFG_ARM64_core=y
+
+echo "Exit coverity_build.sh"
