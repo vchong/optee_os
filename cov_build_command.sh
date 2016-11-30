5e53e191f7f781d9ce57182c1c82b33b37118e94
Victor Chong <victor.chong@linaro.org>
2016-11-30 08:25:42 +0000

    dont use -s for make

--- cov_build_command.sh
+++ cov_build_command.sh
@@ -10,16 +10,16 @@ export PATH=${PWD}/gcc-linaro-5.3-2016.02-x86_64_aarch64-linux-gnu/bin:${PATH}
 export MYHOME=$PWD
 export DST_KERNEL=$PWD/linux
 export DL_DIR=$HOME/downloads
-export make="make -j3 -s"
+export make="make -j3"
 export PATH=${HOME}/bc-1.06/bc:$PATH
 export PATH=$HOME/inst/bin:$PATH
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/git-2.9.3/:$DST_KERNEL/scripts/:$PATH
 
-# QEMU-virt (PLATFORM=vexpress-qemu_virt)
-$make
-
 # HiKey board (HiSilicon Kirin 620)
 $make PLATFORM=hikey CFG_ARM64_core=y
+
+# QEMU-virt (PLATFORM=vexpress-qemu_virt)
+$make
 
 echo "Exit coverity_build.sh"
