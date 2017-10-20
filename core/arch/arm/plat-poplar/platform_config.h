838e4f12d51cf9538d42597451995eb817567b36
Victor Chong <victor.chong@linaro.org>
2017-10-21 00:58:39 +0900

    poplar: Add initial support
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

---
+++ core/arch/arm/plat-poplar/platform_config.h
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+/* Make stacks aligned to data cache line length */
+#define STACK_ALIGNMENT		64
+
+/* PL011 UART */
+#define CONSOLE_UART_BASE	PL011_UART0_BASE
+#define CONSOLE_BAUDRATE	PL011_BAUDRATE
+#define CONSOLE_UART_CLK_IN_HZ	PL011_UART0_CLK_IN_HZ
+
+/*
+ * Poplar memory map
+ *
+ * Note: the physical address ranges below correspond to DRAM which is
+ * non-secure by default. Therefore, the terms TZDRAM and TZSRAM may not
+ * reflect the reality and only indicate areas that "would normally be"
+ * secure DRAM and secure SRAM in a more complete implementation.
+ * The memory map was defined like this for lack of better documentation.
+ * It is good enough for development/testing purposes.
+ *
+ *  0xFF00_0000 [DRAM2_LIMIT]
+ *    other (devmem)
+ *  0xF000_0000 [DRAM2_BASE]
+
+ *  0x4000_0000 [DRAM0_LIMIT]
+ *    u-boot: 144 MiB?
+ *  0x3700_0000 CONFIG_SYS_TEXT_BASE (defined in u-boot)
+ *    ramdisk: 76 MiB?
+ *  0x3240_0000
+ *    fdt: 2 MiB
+ *  0x3220_0000
+ *    pxe file or script addr: 2 MiB
+ *  0x3200_0000
+ *    kernel/android: 32 MiB
+ *  0x3000_0000
+
+ *  0x0280_0000 CONFIG_SYS_LOAD_ADDR (defined in u-boot)
+ *    other: 6 MiB
+ *  0x0220_0000 CONFIG_SYS_INIT_SP_ADDR (defined in u-boot)
+ *  0x0208_0000 KERNEL_TEXT_OFFSET (defined in u-boot)
+ *    unused: 512 KiB
+ *  0x0200_0000
+
+ *  0x0200_0000                                  -
+ *    TA RAM: 14 MiB                             | TZDRAM
+ *  0x0120_0000                                  -
+
+ * CFG_WITH_PAGER=n                              -
+ *    TEE RAM: 2 MiB (CFG_TEE_RAM_VA_SIZE)       | TZDRAM
+ *  0x0100_0000 [TZDRAM_BASE, CFG_TEE_LOAD_ADDR] -
+ *
+ * CFG_WITH_PAGER=y
+ *    Unused
+ *  0x010A_0000                                  -
+ *    TEE RAM: 640 KiB (TZSRAM_SIZE)             | TZSRAM
+ *  0x0100_0000 [TZSRAM_BASE, CFG_TEE_LOAD_ADDR] -
+
+ *    Secure Data Path buffers: 4 MiB
+ *  0x00C0_0000 [CFG_TEE_SDP_MEM_BASE]
+ *    OP-TEE Future Use: 4 MiB
+ *  0x0080_0000
+ *    Shared memory: 4 MiB
+ *  0x0040_0000
+
+ *  0x0040_0000
+ *    unused: 64 KiB
+ *  0x003F_0000
+ *    reserved for uefi persistent values: 1984 KiB
+ *  0x0020_0000
+ *    u-boot persistent values: 64 KiB (CONFIG_ENV_SIZE (defined in u-boot))
+ *  0x001F_0000 l-loader limit (len/size set by poplar-l-loader.git)
+ *              also CONFIG_ENV_OFFSET (defined in u-boot)
+ *    unused (cannot be used)
+ *  0x0010_0000 l-loader limit (max bootrom can accept)
+ *    fip.bin load zone: 768 KiB
+ *  0x0004_0000
+ *    bl31: 80 KiB
+ *  0x002A_0000
+ *    bl2: 48 KiB
+ *  0x0001_E000
+ *    bl1: 64 KiB
+ *  0x0000_E000
+ *    l-loader text: 52 KiB
+ *  0x0000_1000
+
+ *    unused
+ *  0x0000_0000 [DRAM0_BASE]
+ */
+#define DRAM0_BASE		0x00000000
+#define DRAM0_SIZE		0x40000000
+
+#define DRAM0_BASE_NSEC	0x02080000
+#define DRAM0_SIZE_NSEC	(DRAM0_SIZE - DRAM0_BASE_NSEC)
+
+#define DRAM2_BASE		0xF0000000
+#define DRAM2_SIZE		0x0F000000
+
+#ifdef CFG_WITH_PAGER
+
+#define TZSRAM_BASE		0x01000000
+#define TZSRAM_SIZE		CFG_CORE_TZSRAM_EMUL_SIZE
+
+#define TZDRAM_BASE		0x01200000
+#define TZDRAM_SIZE		(14 * 1024 * 1024)
+
+#define CFG_TEE_RAM_START	TZSRAM_BASE
+#define CFG_TEE_RAM_PH_SIZE	TZSRAM_SIZE
+#define CFG_TA_RAM_START	ROUNDUP(TZDRAM_BASE, CORE_MMU_DEVICE_SIZE)
+#define CFG_TA_RAM_SIZE		ROUNDDOWN(TZDRAM_SIZE, CORE_MMU_DEVICE_SIZE)
+
+#else /* CFG_WITH_PAGER */
+
+#define TZDRAM_BASE		0x01000000
+#define TZDRAM_SIZE		(16 * 1024 * 1024)
+
+#define CFG_TEE_RAM_START	TZDRAM_BASE
+#define CFG_TEE_RAM_PH_SIZE	CFG_TEE_RAM_VA_SIZE
+#define CFG_TA_RAM_START	ROUNDUP((TZDRAM_BASE + CFG_TEE_RAM_VA_SIZE), \
+					CORE_MMU_DEVICE_SIZE)
+
+#define CFG_TA_RAM_SIZE		ROUNDDOWN((TZDRAM_SIZE - CFG_TEE_RAM_VA_SIZE),\
+					CORE_MMU_DEVICE_SIZE)
+
+#endif /* CFG_WITH_PAGER */
+
+#define CFG_SHMEM_START		0x00400000
+#define CFG_SHMEM_SIZE		(4 * 1024 * 1024)
+
+#define CFG_TEE_CORE_NB_CORE	4
+
+#define CFG_TEE_RAM_VA_SIZE	(2 * 1024 * 1024)
+
+#define CFG_TEE_LOAD_ADDR	0x01000000 /* BL32_BASE */
+
+#endif /* PLATFORM_CONFIG_H */
