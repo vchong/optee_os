ce6bc815113dc4db0fd25763370bb67be101a9ac
Mykhailo Sopiha <mykhailo.sopiha@linaro.org>
2019-10-24 16:36:41 +0300

    Introduce ASN1 ENUMERATED tag support
    Signed-off-by: Mykhailo Sopiha <mykhailo.sopiha@linaro.org>

--- lib/libmbedtls/mbedtls/library/asn1write.c
+++ lib/libmbedtls/mbedtls/library/asn1write.c
@@ -257,6 +257,32 @@ int mbedtls_asn1_write_int( unsigned char **p, unsigned char *start, int val )
     return( (int) len );
 }
 
+int mbedtls_asn1_write_enum( unsigned char **p, unsigned char *start, int val )
+{
+    int ret;
+    size_t len = 0;
+
+    if( *p - start < 1 )
+	return( MBEDTLS_ERR_ASN1_BUF_TOO_SMALL );
+
+    len += 1;
+    *--(*p) = val;
+
+    if( val > 0 && **p & 0x80 )
+    {
+	if( *p - start < 1 )
+	    return( MBEDTLS_ERR_ASN1_BUF_TOO_SMALL );
+
+	*--(*p) = 0x00;
+	len += 1;
+    }
+
+    MBEDTLS_ASN1_CHK_ADD( len, mbedtls_asn1_write_len( p, start, len ) );
+    MBEDTLS_ASN1_CHK_ADD( len, mbedtls_asn1_write_tag( p, start, MBEDTLS_ASN1_ENUMERATED ) );
+
+    return( (int) len );
+}
+
 int mbedtls_asn1_write_tagged_string( unsigned char **p, unsigned char *start, int tag,
     const char *text, size_t text_len )
 {
