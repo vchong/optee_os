fb64276b9636f950e355cb5cfa54518b17629b2d
Victor Chong <victor.chong@linaro.org>
2020-05-06 09:29:32 +0100

    fixup! hack: fix clang aosp build

--- mk/clang.mk
+++ mk/clang.mk
@@ -3,22 +3,15 @@
 # components are ignored, as well as any command before the compiler name (for
 # instance "/some/path/ccache /other/path/arm-linux-gnueabihf-").
 # We try to extract any ccache command if present.
-#$(info $$sm is [${sm}])
 clang-target	:= $(patsubst %-,%,$(notdir $(lastword $(CROSS_COMPILE_$(sm)))))
-#$(info $$clang-target is [${clang-target}])
 ccache-cmd	:= $(if $(findstring ccache,$(CROSS_COMPILE_$(sm))),$(firstword $(CROSS_COMPILE_$(sm))) ,)
-#$(info $$ccache-cmd is [${ccache-cmd}])
 
 #CC$(sm)2		:= $(ccache-cmd)clang --target=aarch4-linux-android
-#$(info $$CC${sm}2 is [${CC${sm}2}])
 CC$(sm)		:= $(ccache-cmd)clang --target=$(clang-target)
 # Due to the absence of clang-cpp in AOSP's prebuilt version of clang,
 # use the equivalent command of 'clang -E'
-#$(info $$CC${sm} is [${CC${sm}}])
 CPP$(sm)	:= $(ccache-cmd)clang --target=$(clang-target) -E
-#$(info $$CPP${sm} is [${CPP${sm}}])
 LD$(sm)		:= $(ccache-cmd)ld.lld
-#$(info $$LD${sm} is [${LD${sm}}])
 
 AR$(sm)		:= $(ccache-cmd)llvm-ar
 NM$(sm)		:= llvm-nm
@@ -32,14 +25,10 @@ nostdinc$(sm)	:= -nostdinc -isystem $(shell $(CC$(sm)) \
 comp-cflags-warns-clang := -Wno-language-extension-token \
 			 -Wno-gnu-zero-variadic-macro-arguments
 
-#$(info $$arch-bits-${sm} is [${arch-bits-${sm}}])
-#$(info $$CFLAGS${arch-bits-${sm}} is [${CFLAGS${arch-bits-${sm}}}])
-#$(info $$comp-cflags${sm} is [${comp-cflags${sm}}])
 # Note, the currently used compiler runtime library may be libgcc.a or
 # libclang_rt.builtins.*.a depending on the compiler build-time configuration.
-libgcc$(sm)  	:= $(shell $(CC$(sm)) $(CFLAGS$(arch-bits-$(sm))) $(comp-cflags$(sm)) \
+libgcc$(sm)	:= $(shell $(CC$(sm)) $(CFLAGS$(arch-bits-$(sm))) $(comp-cflags$(sm)) \
 			-rtlib=compiler-rt -print-libgcc-file-name 2> /dev/null)
-#$(info $$libgcc${sm}  is [${libgcc${sm}}])
 
 # Core ASLR relies on the executable being ready to run from its preferred load
 # address, because some symbols are used before the MMU is enabled and the
@@ -56,5 +45,5 @@ OBJCOPY		:= false
 OBJDUMP		:= false
 READELF		:= false
 nostdinc	:= --bad-nostdinc-variable
-libgcc  	:= --bad-libgcc-variable
+libgcc		:= --bad-libgcc-variable
 
