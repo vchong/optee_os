1e53ca1416d7e4148f73965e35e148fa1eba4762
Victor Chong <victor.chong@linaro.org>
2020-05-29 19:40:36 +0100

    wip

--- mk/android_flags.mk
+++ mk/android_flags.mk
@@ -6,9 +6,9 @@ OPTEE_EXTRA_FLAGS += CFG_CORE_HEAP_SIZE=196608
 endif
 
 # https://docs.google.com/document/d/1YhXpsmefaMhWE4dIwPViG0lzfCKVkf1WqjvknyPh7Pw
-+# core crash (alignment fault) when looking up early TAs (memcmp on UUIDs)
-+# https://github.com/OP-TEE/optee_os/issues/3903
-CFG_SCTLR_ALIGNMENT_CHECK=n
+# core crash (alignment fault) when looking up early TAs (memcmp on UUIDs)
+# https://github.com/OP-TEE/optee_os/issues/3903
+CFG_SCTLR_ALIGNMENT_CHECK = n
 
 # Force DEBUG=0 for now else boot will hang (https://pastebin.ubuntu.com/p/jX3yfQ2xYV/)
 OPTEE_EXTRA_FLAGS += DEBUG=0
