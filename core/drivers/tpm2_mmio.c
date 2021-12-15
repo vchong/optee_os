d3ade7e31fa6767d218ac608e20e4cd6801b6543
Victor Chong <victor.chong@linaro.org>
2021-12-16 02:22:23 +0900

    wip

--- core/drivers/tpm2_mmio.c
+++ core/drivers/tpm2_mmio.c
@@ -17,3 +17,33 @@
 #include <trace.h>
 #include <util.h>
 
+static vaddr_t *chip_to_base(struct tpm2_chip *chip)
+{
+	struct tpm2_mmio_data *md =
+		container_of(chip, struct tpm2_mmio_data, chip);
+
+	return io_pa_or_va(&md->base, MMIO_REG_SIZE);
+}
+
+static enum tpm2_result tpm2_mmio_rx8(tpm2_chip *chip, uint32_t adr,
+				      uint16_t len, uint8_t *buf)
+{
+	vaddr_t base = chip_to_base(chip);
+
+	while (len--)
+		*buf++ = io_read8(base + adr);
+
+	return TPM2_OK;
+}
+
+static enum tpm2_result tpm2_mmio_tx8(tpm2_chip *chip, uint32_t adr,
+				      uint16_t len, uint8_t *buf)
+{
+	vaddr_t base = chip_to_base(chip);
+
+	while (len--)
+		io_write8(base + adr, *buf++);
+
+	return TPM2_OK;
+}
+
--- core/include/drivers/tpm2_mmio.h
+++ core/include/drivers/tpm2_mmio.h
@@ -9,11 +9,14 @@
 
 #include <tpm2.h>
 
+/* not sure about this */
+#define MMIO_REG_SIZE	0x1000
+
 struct tpm2_mmio_data {
 	struct tpm2_chip *chip;	
 	uint32_t pcr_count;
 	uint32_t pcr_select_min;
-	vaddr_t tpm2_base;
+	vaddr_t base;
 }
 
 #endif	/* __TPM2_MMIO_H__ */
